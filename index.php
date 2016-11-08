576c1dee5609136a194899e4ad0cd9b868737589
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-11-08 22:16:19 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Money Tacker</title>
+	</head>
+
+	<body>
+		<?php
+			include ("connect.php");
+			
+			try {
+				$conn = new PDO("mysql:host=$host;dbname=$dbname", $login, $password);
+				// set the PDO error mode to exception
+				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				echo "Connected successfully";
+			} catch(PDOException $erreur) {
+				echo "<p>Erreur ".$erreur->getMessage()."</p>\n";
+			}			
+		?>
+	</body>
+</html>
---
+++ sql/create_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE records (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	time INTEGER,
+	type INTEGER,
+	title TEXT,
+	category INTEGER,
+	price INTEGER,
+	account_id INTEGER,
+	currency TEXT,
+	user_id INTEGER,
+	PRIMARY KEY (id)
+);
+	
+CREATE TABLE users (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	created_at INTEGER,
+	full_name TEXT,
+	email TEXT,
+	PRIMARY KEY (id)
+);
+	
