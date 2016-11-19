9f189406bf3ebf51fa33fd3cf6d5d4162c79eecb
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-11-19 13:13:26 +0200

    #11[1h]. Add some CSS>

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 				<a class = "link_add_expense" href="add_record.php?action=add&type=1">Add expense</a>
 	      	</div>
 			
-			<div class = "container">
+			<div class = "period_container">
 				<form>
 					<?php
 						$dateFrom = isset($_GET['date_from']) ? $_GET['date_from'] : date('Y-m-01');
@@ -38,6 +38,15 @@
 						echo '<input type="date" name="date_to" value="'.$dateTo.'">'; 
 					?>
 					<input type="submit" value="Display records"/>
+					<?php
+						$dateFrom = isset($_GET['date_from']) ? $_GET['date_from'] : date('Y-m-01');
+						$dateTo = isset($_GET['date_to']) ? $_GET['date_to'] : date("Y-m-t", strtotime(date('Y-m-01')));
+						
+						$ts_from = strtotime($dateFrom);
+						$ts_to = strtotime($dateTo) + (24 * 60 * 60 - 1);
+						
+						echo '<a href="report.php?ts_from='.$ts_from.'&ts_to='.$ts_to.'">Report</a>';
+					?>
 				<form>
 			</div>
 			
@@ -53,19 +62,18 @@
 							return '<a href="delete_record.php?record_id='.$record['record_id'].'">Delete</a>';
 						}
 						
-						function displayRecords($conn, $ts_from, $ts_to) {
+						function getRecords($conn, $ts_from, $ts_to) {
 							$ts_to += (24 * 60 * 60 - 1);
 							$sql = 'SELECT records.id AS record_id, type, time, records.title AS title, categories.title AS category, price 
 									FROM records, categories
 									WHERE user_id='.$_SESSION['user_id'].
 									' AND category_id=categories.id'.
 									' AND time BETWEEN '.$ts_from.' AND '.$ts_to.';';
-							
-							$totalIncome = 0;
-							$totalExpense = 0;
-							
+							return $conn->query($sql);
+						}
+						
+						function displayRecords($conn, $ts_from, $ts_to) {							
 							echo '<table>
-									<caption>Records</caption>
 									<tr>
 										<th>Time</th>
 										<th>Title</th>
@@ -73,13 +81,11 @@
 										<th>Price</th>
 									</tr>';
 									
-							foreach ($conn->query($sql) as $row) {
+							foreach (getRecords($conn, $ts_from, $ts_to) as $row) {
 								$recordClass = "";
 								if ($row['type'] == 0) {
-									$totalIncome += $row['price'];
 									$recordClass = "record_income";
 								} else {
-									$totalExpense += $row['price'];
 									$recordClass = "record_expense";
 								}
 								echo '<tr class="'.$recordClass.'">
@@ -93,12 +99,6 @@
 							}
 							
 							echo '</table>';
-							
-							echo '<h2>Short summary</h2>';
-							echo '<p>Total income: '.$totalIncome.'</p>';
-							echo '<p>Total expense: '.$totalExpense.'</p>';
-							echo '<p>Total: '.($totalIncome - $totalExpense).'</p>';
-							echo '<a href="report.php?ts_from='.$ts_from.'&ts_to='.$ts_to.'">Report</a>';
 						}
 						
 						try {
@@ -108,7 +108,7 @@
 
 							$dateFrom = isset($_GET['date_from']) ? $_GET['date_from'] : date('Y-m-01');
 							$dateTo = isset($_GET['date_to']) ? $_GET['date_to'] : date("Y-m-t", strtotime(date('Y-m-01')));
-
+							
 							displayRecords($conn, strtotime($dateFrom), strtotime($dateTo));
 						} catch(PDOException $error) {
 							echo "<p>Error: ".$error->getMessage()."</p>\n";
@@ -117,7 +117,7 @@
 			</div>
 
 			<div class = "footer">
-				<p>Created by Evgenii Kanivets and Elena Martyk</p>
+				<p>Created by Evgenii Kanivets and Elena Martiuk</p>
 			</div>
 		</div>
 	</body>
--- report.php
+++ report.php
@@ -6,74 +6,113 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Money Tacker</title>
+		<title>Report</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
 
 	<body>
-		<div class = "container">
-         <?php
-			include ("connect.php");
+		<div class="main_container">
+			<div class = "header">
+				<?php echo "<p class >Welcome, ".$_SESSION['username']."!</p>"; ?>
+				<a href="logout.php">Logout</a>
+			</div>
+			<div class = "container">
+			 <?php
+				include ("connect.php");
 
-			function displayReport($conn, $ts_from, $ts_to) {
-						$sql = 'SELECT records.id AS record_id, type, time, records.title AS title, categories.title AS category, price 
-								FROM records, categories
-								WHERE user_id='.$_SESSION['user_id'].
-								' AND category_id=categories.id'.
-								' AND time BETWEEN '.$ts_from.' AND '.$ts_to.';';
-						
-						$totalIncome = 0;
-						$totalExpense = 0;
+				function getRecords($conn, $ts_from, $ts_to) {
+					$ts_to += (24 * 60 * 60 - 1);
+					$sql = 'SELECT records.id AS record_id, type, time, records.title AS title, categories.title AS category, price 
+							FROM records, categories
+							WHERE user_id='.$_SESSION['user_id'].
+							' AND category_id=categories.id'.
+							' AND time BETWEEN '.$ts_from.' AND '.$ts_to.';';
+					return $conn->query($sql);
+				}
+				
+				function displayReport($conn, $ts_from, $ts_to) {
+					$sql = 'SELECT records.id AS record_id, type, time, records.title AS title, categories.title AS category, price 
+							FROM records, categories
+							WHERE user_id='.$_SESSION['user_id'].
+							' AND category_id=categories.id'.
+							' AND time BETWEEN '.$ts_from.' AND '.$ts_to.';';
+							
+					$totalIncome = 0;
+					$totalExpense = 0;
 
-						$report = array();
-						foreach ($conn->query($sql) as $row) {
-							if (!isset($report[$row['category']])) {
-								$report[$row['category']] = 0;
-							}
-							if ($row['type'] == 0) {
-								$totalIncome += $row['price'];
-								$report[$row['category']] += $row['price'];
-							} else {
-								$totalExpense += $row['price'];
-								$report[$row['category']] -= $row['price'];
-							}
+					$report = array();
+					foreach (getRecords($conn, $ts_from, $ts_to) as $row) {
+						if (!isset($report[$row['category']])) {
+							$report[$row['category']] = 0;
 						}
-						
-						echo '<table>
-								<caption>Records</caption>
-								<tr>
-									<th>Category</th>
-									<th>Total</th>
-								</tr>';
-								
-						foreach ($report as $category => $total) {
-							echo '<tr>
-									<td>'.$category.'</td>
-									<td>'.$total.'</td>
-								 </tr>';
+						if ($row['type'] == 0) {
+							$totalIncome += $row['price'];
+							$report[$row['category']] += $row['price'];
+						} else {
+							$totalExpense += $row['price'];
+							$report[$row['category']] -= $row['price'];
 						}
-						
-						echo '</table>';
-						
-						echo '<h2>Short summary</h2>';
-						echo '<p>Total income: '.$totalIncome.'</p>';
-						echo '<p>Total expense: '.$totalExpense.'</p>';
-						echo '<p>Total: '.($totalIncome - $totalExpense).'</p>';
 					}
-		 
-            if (!isset($_SESSION['user_id'])) {
-				header('Location: '.'sign_in.php', true, $permanent ? 301 : 302);
-				exit();
-			}
-			
-			try {
-                $conn = new PDO("mysql:host=$host;dbname=$dbname", $login, $password);
-                // set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                displayReport($conn, $_GET['ts_from'], $_GET['ts_to']);
-            } catch(PDOException $error) {
-                echo "<p>Error: ".$error->getMessage()."</p>\n";
-            }
-         ?>
-      	</div>
+					
+					echo '<table>
+							<tr>
+								<th>Category</th>
+								<th>Price</th>
+							</tr>';
+					
+					foreach ($report as $category => $total) {
+						$recordClass = "";
+						if ($row['type'] == 0) {
+							$recordClass = "record_income";
+						} else {
+							$recordClass = "record_expense";
+						}
+						echo '<tr class="'.$recordClass.'">
+								<td>'.$category.'</td>
+								<td>'.$total.'</td>
+							 </tr>';
+					}
+					
+					echo '</table>';
+				}
+							
+				function displayShortOverview($conn, $ts_from, $ts_to) {
+					$totalIncome = 0;
+					$totalExpense = 0;
+										
+					foreach (getRecords($conn, $ts_from, $ts_to) as $row) {
+						if ($row['type'] == 0) {
+							$totalIncome += $row['price'];
+						} else {
+							$totalExpense += $row['price'];
+						}
+					}
+								
+					echo '<h2>Short summary</h2>
+						  <p>Total income: '.$totalIncome.'<br>
+						  Total expense: '.$totalExpense.'<br>
+						  Total: '.($totalIncome - $totalExpense).'</p>';
+				}
+			 
+				if (!isset($_SESSION['user_id'])) {
+					header('Location: '.'sign_in.php', true, $permanent ? 301 : 302);
+					exit();
+				}
+				
+				try {
+					$conn = new PDO("mysql:host=$host;dbname=$dbname", $login, $password);
+					// set the PDO error mode to exception
+					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					displayShortOverview($conn, $_GET['ts_from'], $_GET['ts_to']);
+					displayReport($conn, $_GET['ts_from'], $_GET['ts_to']);
+				} catch(PDOException $error) {
+					echo "<p>Error: ".$error->getMessage()."</p>\n";
+				}
+			 ?>
+			</div>
+			<div class = "footer">
+				<p>Created by Evgenii Kanivets and Elena Martiuk</p>
+			</div>
+		</div>
 	</body>
 </html>
