2ffa63c1d9886550e3bb12cc5ee441aa005f5c1d
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2016-11-21 13:40:44 +0200

    Change connect info to work with Heroku/

--- connect.php
+++ connect.php
@@ -1,6 +1,8 @@
 <?php
-	$host = "localhost";
-	$login = "root";
-	$password = "";
-	$dbname = "money_tracker"; 
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 ?>
