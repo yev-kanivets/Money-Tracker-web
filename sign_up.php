90d0a61104442736269a5c7e04376e6ecb2d35ae
Elenamart <helene.martiuk@gmail.com>
2016-11-22 18:50:16 +0200

    translate sign up page

--- sign_up.php
+++ sign_up.php
@@ -7,7 +7,7 @@
 <html>
    <head>
       <meta charset="utf-8">
-      <title>Sign up on Money Tacker</title>
+      <title>Signer au Compteur d'argent</title>
       <link rel="stylesheet" type="text/css" href="css/login.css">
    </head>
    <body>
@@ -36,16 +36,16 @@
                   $error = "";
 
                   if (strlen($full_name) == 0) {
-                     $error = "Full name can't be empty.";
+                     $error = "Nom ne doit être vide.";
                   }
                   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                     $error = "Email is invalid.";
+                     $error = "E-mail n'est pas valable.";
                   }
                   if (strlen($password) < 6 || $password != $confirm_password) {
-                     $error = "Passwords must be equal and length > 5.";
+                     $error = "Mots de passe doivent être éguaux et longueur > 5.";
                   }
                   if (isUserExists($conn, $email)) {
-                     $error = "User with such email already exists.";
+                     $error = "Usager avec cet email existe déjà.";
                   }
 
                   if ($error == "") {
@@ -67,7 +67,7 @@
                      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                      signUp($conn, $_POST['full_name'], $_POST['email'], $_POST['password'], $_POST['confirm_password']);
                   } catch(PDOException $error) {
-                     echo "<p>Error: ".$error->getMessage()."</p>\n";
+                     echo "<p>Erreur: ".$error->getMessage()."</p>\n";
                   }
                }
             ?>
@@ -75,15 +75,15 @@
          
          <div class = "container">
             <form action="sign_up.php" method="post">
-               <h3>Sign up on Money Tracker</h3>
+               <h3>Signer au Compteur d'argent</h3>
                <fieldset>
                   <input type="hidden" name="action" value="action_sign_up" />
-                  <p><input type="text" name="full_name" size="40" maxlength="40" placeholder="Full name" /></p>
-                  <p><input type="email" name="email" size="40" maxlength="40" placeholder="Email" /></p>
-                  <p><input type="password" name="password" size="40" maxlength="40" placeholder="Password" /></p>
-                  <p><input type="password" name="confirm_password" size="40" maxlength="40" placeholder="Confirm password" /></p>
-                  <p><input type="submit" name="sign_up" value="Sign Up" /></p>
-                  <a href="sign_in.php">Already have an account? Sign In</a>
+                  <p><input type="text" name="full_name" size="40" maxlength="40" placeholder="Nom" /></p>
+                  <p><input type="email" name="email" size="40" maxlength="40" placeholder="E-mail" /></p>
+                  <p><input type="password" name="password" size="40" maxlength="40" placeholder="Mot de passe" /></p>
+                  <p><input type="password" name="confirm_password" size="40" maxlength="40" placeholder="Confirmer le mot de passe" /></p>
+                  <p><input type="submit" name="sign_up" value="Signer" /></p>
+                  <a href="sign_in.php">Avez-vous déjà un compte? Entrer</a>
                </fieldset>
             </form>
          </div>
